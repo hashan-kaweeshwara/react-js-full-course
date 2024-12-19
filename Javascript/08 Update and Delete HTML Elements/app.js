da89d6603bcabe06e1e8f9ef62e23b742d7448a2
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2024-12-20 01:46:14 +0530

    new

---
+++ Javascript/08 Update and Delete HTML Elements/app.js
@@ -0,0 +1,38 @@
+var btnAdd = document.getElementById("add-item");
+var list = document.getElementById("todo-list");
+var inputBox = document.getElementById("todo-input");
+var btnUpdate = document.getElementById("update-item");
+
+var currentInputValue = "";
+inputBox.addEventListener("input", function (e) {
+  currentInputValue = e.target.value;
+});
+
+inputBox.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    addListItem();
+  }
+});
+
+function addListItem() {
+  if (
+    currentInputValue !== undefined &&
+    currentInputValue !== null &&
+    currentInputValue !== ""
+  ) {
+    var newListElement = document.createElement("li");
+    var textNode = document.createTextNode(currentInputValue);
+    newListElement.appendChild(textNode);
+    newListElement.id = "item" + (list.childElementCount + 1);
+
+    list.appendChild(newListElement);
+    console.log(list.childElementCount);
+
+    inputBox.value = "";
+    currentInputValue = "";
+  } else {
+    alert("Please enter a valid todo item");
+  }
+}
+
+btnAdd.addEventListener("click", addListItem);
