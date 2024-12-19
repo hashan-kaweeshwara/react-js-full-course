5027833e19f0e5991a765cf7f86ea949bddf577e
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2024-12-19 21:59:25 +0530

    new

--- Javascript/07 Create HTML Elements/app.js
+++ Javascript/07 Create HTML Elements/app.js
@@ -1,18 +1,31 @@
 var btnAdd = document.getElementById("add-item");
 var list = document.getElementById("todo-list");
+var inputBox = document.getElementById("todo-input");
+
+var currentInputValue = "";
+inputBox.addEventListener("input", function (e) {
+  currentInputValue = e.target.value;
+});
+
+inputBox.addEventListener("keyup", func);
 
 btnAdd.addEventListener("click", function () {
-  var newListElement = document.createElement("li");
-  var textNode = document.createTextNode(
-    "List Item " + (list.childElementCount + 1)
-  );
-  newListElement.appendChild(textNode);
-  newListElement.id = "item" + (list.childElementCount + 1);
+  if (
+    currentInputValue !== undefined &&
+    currentInputValue !== null &&
+    currentInputValue !== ""
+  ) {
+    var newListElement = document.createElement("li");
+    var textNode = document.createTextNode(currentInputValue);
+    newListElement.appendChild(textNode);
+    newListElement.id = "item" + (list.childElementCount + 1);
 
-  var firstItem = list.firstElementChild;
-  console.log(firstItem);
+    list.appendChild(newListElement);
+    console.log(list.childElementCount);
 
-  // list.appendChild(newListElement);
-  list.insertBefore(newListElement, firstItem);
-  console.log(list.childElementCount);
+    inputBox.value = "";
+    currentInputValue = "";
+  } else {
+    alert("Please enter a valid todo item");
+  }
 });
