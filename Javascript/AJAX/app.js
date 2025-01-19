81644672d55e836114db4e38597db1b3b52d5052
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2025-01-19 15:16:22 +0530

    new

--- Javascript/AJAX/app.js
+++ Javascript/AJAX/app.js
@@ -1,9 +1,21 @@
+function createTODODynamically(id, title) {
+  var newListElement = document.createElement("li");
+  var textNode = document.createTextNode("title");
+  newListElement.appendChild(textNode);
+  newListElement.id = id;
+
+  return newListElement;
+}
+
 function getTODOListFromBackend() {
   var http = new XMLHttpRequest();
   http.onreadystatechange = function () {
     if (this.readyState === 4) {
       if (this.status === 200) {
-        console.log(JSON.parse(this.responseText));
+        var response = JSON.parse(this.responseText);
+        for (var i = 0; i < response.length; i++) {
+          console.log(createTODODynamically(response[i].id, response[i].title));
+        }
       } else {
         console.log("Call Failed");
       }
