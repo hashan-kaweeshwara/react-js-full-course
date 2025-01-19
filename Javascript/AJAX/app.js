8b34349f8d44e8921c99bbdfa343a212d54b6153
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2025-01-19 16:34:40 +0530

    new

--- Javascript/AJAX/app.js
+++ Javascript/AJAX/app.js
@@ -1,3 +1,62 @@
+var btnAdd = document.getElementById("add-item");
+var list = document.getElementById("todo-list");
+var inputBox = document.getElementById("todo-input");
+var btnUpdate = document.getElementById("update-item");
+var btnRemove = document.getElementById("remove-item");
+
+var currentInputValue = "";
+inputBox.addEventListener("input", function (e) {
+  currentInputValue = e.target.value;
+});
+
+inputBox.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    addListItem();
+  }
+});
+
+function createNewNode() {
+  var newListElement = document.createElement("li");
+  var textNode = document.createTextNode(currentInputValue);
+  newListElement.appendChild(textNode);
+  newListElement.id = "item" + (list.childElementCount + 1);
+
+  return newListElement;
+}
+
+function addListItem() {
+  if (
+    currentInputValue !== undefined &&
+    currentInputValue !== null &&
+    currentInputValue !== ""
+  ) {
+    var newListElement = createNewNode();
+
+    list.appendChild(newListElement);
+    console.log(list.childElementCount);
+
+    inputBox.value = "";
+    currentInputValue = "";
+  } else {
+    alert("Please enter a valid todo item");
+  }
+}
+
+btnAdd.addEventListener("click", addListItem);
+
+btnUpdate.addEventListener("click", function () {
+  var firstElement = list.firstElementChild;
+  var newListElement = createNewNode();
+
+  list.replaceChild(newListElement, firstElement);
+});
+
+btnRemove.addEventListener("click", function () {
+  var firstElement = list.firstElementChild;
+
+  list.removeChild(firstElement);
+});
+
 function createTODODynamically(id, title) {
   var newListElement = document.createElement("li");
   var textNode = document.createTextNode("title");
@@ -14,7 +73,9 @@ function getTODOListFromBackend() {
       if (this.status === 200) {
         var response = JSON.parse(this.responseText);
         for (var i = 0; i < response.length; i++) {
-          console.log(createTODODynamically(response[i].id, response[i].title));
+          list.appendChild(
+            createTODODynamically(response[i].id, response[i].title)
+          );
         }
       } else {
         console.log("Call Failed");
