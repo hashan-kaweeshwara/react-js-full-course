e743560cae973af41d4d724902b21dc2cc589637
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2025-01-21 14:29:19 +0530

    new

---
+++ JQuery/Selectors and Filters/app.js
@@ -0,0 +1,114 @@
+var btnAdd = document.getElementById("add-item");
+var list = document.getElementById("todo-list");
+var inputBox = document.getElementById("todo-input");
+var btnUpdate = document.getElementById("update-item");
+var btnRemove = document.getElementById("remove-item");
+
+var currentInputValue = "";
+inputBox.addEventListener("input", function (e) {
+  currentInputValue = e.target.value;
+});
+
+inputBox.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    addListItem();
+  }
+});
+
+function createNewNode() {
+  var newListElement = document.createElement("li");
+  var textNode = document.createTextNode(currentInputValue);
+  newListElement.appendChild(textNode);
+  newListElement.id = "item" + (list.childElementCount + 1);
+
+  return newListElement;
+}
+
+function addListItem() {
+  if (
+    currentInputValue !== undefined &&
+    currentInputValue !== null &&
+    currentInputValue !== ""
+  ) {
+    var newListElement = createNewNode();
+
+    list.appendChild(newListElement);
+    console.log(list.childElementCount);
+
+    inputBox.value = "";
+    currentInputValue = "";
+  } else {
+    alert("Please enter a valid todo item");
+  }
+}
+
+btnAdd.addEventListener("click", createTODOItemAtBackend);
+
+btnUpdate.addEventListener("click", function () {
+  var firstElement = list.firstElementChild;
+  var newListElement = createNewNode();
+
+  list.replaceChild(newListElement, firstElement);
+});
+
+btnRemove.addEventListener("click", function () {
+  var firstElement = list.firstElementChild;
+
+  list.removeChild(firstElement);
+});
+
+function createTODODynamically(id, title) {
+  var newListElement = document.createElement("li");
+  var textNode = document.createTextNode(title);
+  newListElement.appendChild(textNode);
+  newListElement.id = id;
+
+  return newListElement;
+}
+
+function getTODOListFromBackend() {
+  var http = new XMLHttpRequest();
+  http.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        var response = JSON.parse(this.responseText);
+        for (var i = 0; i < response.length; i++) {
+          list.appendChild(
+            createTODODynamically(response[i].id, response[i].title)
+          );
+        }
+      } else {
+        console.log("Call Failed");
+      }
+    }
+  };
+  http.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+  http.send();
+}
+
+getTODOListFromBackend();
+
+function createTODOItemAtBackend() {
+  var http = new XMLHttpRequest();
+  http.open("POST", "https://jsonplaceholder.typicode.com/todos", true);
+  http.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 201) {
+        var response = JSON.parse(this.responseText);
+        LargestContentfulPaint.appendChild(
+          createTODODynamically(response.id, currentInputValue)
+        );
+        console.log("Add Item to the list");
+      } else {
+        console.log("Call Faild");
+      }
+    }
+  };
+
+  var obj = JSON.stringify({
+    userId: 1,
+    title: currentInputValue,
+    completed: false,
+  });
+  http.send(obj);
+}
