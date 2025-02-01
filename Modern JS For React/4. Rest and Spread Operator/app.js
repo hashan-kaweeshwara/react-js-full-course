1ee2494049435ef845c3225bc7a015b2eb062a36
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2025-02-01 18:57:42 +0530

    new

--- Modern JS For React/4. Rest and Spread Operator/app.js
+++ Modern JS For React/4. Rest and Spread Operator/app.js
@@ -1,5 +1,20 @@
 // const mSum = (num1, num2) => console.log(num1 + num2);
 
-const mSum = (...args) => console.log(args);
+//
 
-mSum(1, 2, 3, 4, 5);
+// const mSum = (...args) => console.log(args);
+
+// mSum(1, 2, 3, 4, 5);
+
+//
+
+const mSum = (...args) => {
+  let sum = 0;
+  for (let i = 0; i < args.length; i++) {
+    sum += args[i];
+  }
+
+  console.log(sum);
+};
+
+mSum(1, 2, 3, 4, 5, 6, 7, 8);
