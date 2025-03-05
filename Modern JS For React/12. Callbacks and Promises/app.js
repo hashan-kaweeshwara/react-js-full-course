2188a80c8fb4e0318a1fb839f9cc19f5c93d681c
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2025-03-05 15:12:27 +0530

    new

---
+++ Modern JS For React/12. Callbacks and Promises/app.js
@@ -0,0 +1,14 @@
+// let list = [];
+
+// $.get(`https://jsonplaceholder.typicode.com/posts`, (response) => {
+//   console.log(response);
+
+//   const id = 1;
+//   $.get(`https://jsonplaceholder.typicode.com/posts/${id}`, (response) => {
+//     console.log(response);
+//   });
+// });
+
+const postListPromise = new Promise(resolve, reject) => {
+  $.get
+};
